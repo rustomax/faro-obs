834967a44e6c4b61b165d7c941aec41e6c29b488
Max Skybin <max.skybin@gmail.com>
2024-02-29 09:34:23 -0800

    Update inject.js

--- inject.js
+++ inject.js
@@ -1,25 +1,28 @@
-window.GrafanaFaroWebSdk.initializeFaro({
-  app: {
-    name: "observeshop",
-  },
-  transports: [
-    new window.GrafanaFaroWebSdk.FetchTransport({
-      url: "https://141741533462.collect.observeinc.com/v1/http?source=faro",
-      requestOptions: {
-        headers: {
-          Authorization:
-            "Bearer ds1DBgMsiblIgCRKJ3kW:euytJ62qlDCsObOD4WsJBPoiWT-2JZPq",
+window.init = () => {
+  window.GrafanaFaroWebSdk.initializeFaro({
+    app: {
+      name: "observeshop",
+    },
+    transports: [
+      new window.GrafanaFaroWebSdk.FetchTransport({
+        url: "https://141741533462.collect.observeinc.com/v1/http?source=faro",
+        requestOptions: {
+          headers: {
+            Authorization:
+              "Bearer ds1DBgMsiblIgCRKJ3kW:euytJ62qlDCsObOD4WsJBPoiWT-2JZPq",
+          },
         },
-      },
-    }),
-  ],
-  sessionTracking: {
-    enabled: false,
-  },
-  batching: {
-    enabled: true,
-  },
-});
+      }),
+    ],
+    sessionTracking: {
+      enabled: true,
+      persistent: true
+    },
+    batching: {
+      enabled: true,
+    },
+  });
+};
 
 window.addTracing = () => {
   window.GrafanaFaroWebSdk.faro.instrumentations.add(new window.GrafanaFaroWebTracing.TracingInstrumentation({}));
